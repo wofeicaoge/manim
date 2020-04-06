337708f77b7c61490d9e91b8b5d87a2d828911de
wofeicaoge <victorydh123@163.com>
2020-04-06 08:44:20 +0800

    revert the changes for example_scenes.py

--- example_scenes.py
+++ example_scenes.py
@@ -1,67 +1,136 @@
+#!/usr/bin/env python
 
 from manimlib.imports import *
-from from_3b1b.on_hold.eop.reusable_imports import *
 
-PRODUCTION_QUALITY_FRAME_DURATION = 0.03
+# To watch one of these scenes, run the following:
+# python -m manim example_scenes.py SquareToCircle -pl
+#
+# Use the flat -l for a faster rendering at a lower
+# quality.
+# Use -s to skip to the end and just save the final frame
+# Use the -p to have the animation (or image, if -s was
+# used) pop up once done.
+# Use -n <number> to skip ahead to the n'th animation of a scene.
+# Use -r <number> to specify a resolution (for example, -r 1080
+# for a 1920x1080 video)
 
-class ShuffleThroughAllSequences(Scene):
-    CONFIG = {
-        "nb_coins" : 20,
-        "run_time" : 5,
-        "fps" : int(1.0/PRODUCTION_QUALITY_FRAME_DURATION),
-        "coin_size" : 0.5,
-        "coin_spacing" : 0.65
-    }
 
-    @Scene.handle_play_like_call
-    def anim(self, nb_relevant_coins, idle_part, left_idle_part, last_coin_seq):
-            for i in range(2**nb_relevant_coins):
-                binary_seq = binary(i)
-                # pad to the left with 0s
-                nb_leading_zeroes = nb_relevant_coins - len(binary_seq)
-                for j in range(nb_leading_zeroes):
-                    binary_seq.insert(0, 0)
-                seq2 = ["H" if x == 0 else "T" for x in binary_seq]
-                coin_seq = CoinSequence(seq2,
-                    radius = self.coin_size * 0.5,
-                    spacing = self.coin_spacing)
-                coin_seq.next_to(idle_part, LEFT, buff = self.coin_spacing - self.coin_size)
-                left_idle_part.next_to(coin_seq, LEFT, buff = self.coin_spacing - self.coin_size)
-                all_coins = VGroup(left_idle_part, coin_seq) #, idle_part)
-                all_coins.center()
-                self.remove(last_coin_seq)
-                self.add(coin_seq)
-                #self.wait(1.0/self.fps)
-                self.update_frame()
-                self.add_frames(self.get_frame())
-                last_coin_seq = coin_seq
-                print(float(i)/2**nb_relevant_coins)
-
+class OpeningManimExample(Scene):
     def construct(self):
+        title = TextMobject("This is some \\LaTeX")
+        basel = TexMobject(
+            "\\sum_{n=1}^\\infty "
+            "\\frac{1}{n^2} = \\frac{\\pi^2}{6}"
+        )
+        VGroup(title, basel).arrange(DOWN)
+        self.play(
+            Write(title),
+            FadeInFrom(basel, UP),
+        )
+        self.wait()
 
-        nb_frames = self.run_time * self.fps
-        nb_relevant_coins = int(np.log2(nb_frames)) + 1
-        print("relevant coins:", nb_relevant_coins)
-        nb_idle_coins = self.nb_coins - nb_relevant_coins
+        transform_title = TextMobject("That was a transform")
+        transform_title.to_corner(UP + LEFT)
+        self.play(
+            Transform(title, transform_title),
+            LaggedStart(*map(FadeOutAndShiftDown, basel)),
+        )
+        self.wait()
 
-        idle_heads = CoinSequence(nb_idle_coins * ["H"],
-            radius = self.coin_size * 0.5,
-            spacing = self.coin_spacing)
-        idle_tails = CoinSequence(nb_idle_coins * ["T"],
-            radius = self.coin_size * 0.5,
-            spacing = self.coin_spacing)
-        idle_tails.fade(0.5)
+        grid = NumberPlane()
+        grid_title = TextMobject("This is a grid")
+        grid_title.scale(1.5)
+        grid_title.move_to(transform_title)
 
-        idle_part = VGroup(idle_heads, idle_tails)
-        left_idle_part = CoinSequence(6 * ["H"],
-            radius = self.coin_size * 0.5,
-            spacing = self.coin_spacing)
+        self.add(grid, grid_title)  # Make sure title is on top of grid
+        self.play(
+            FadeOut(title),
+            FadeInFromDown(grid_title),
+            ShowCreation(grid, run_time=3, lag_ratio=0.1),
+        )
+        self.wait()
 
-        #self.add(idle_part, left_idle_part)
-        self.add(left_idle_part)
-        last_coin_seq = VGroup()
+        grid_transform_title = TextMobject(
+            "That was a non-linear function \\\\"
+            "applied to the grid"
+        )
+        grid_transform_title.move_to(grid_title, UL)
+        grid.prepare_for_nonlinear_transform()
+        self.play(
+            grid.apply_function,
+            lambda p: p + np.array([
+                np.sin(p[1]),
+                np.sin(p[0]),
+                0,
+            ]),
+            run_time=3,
+        )
+        self.wait()
+        self.play(
+            Transform(grid_title, grid_transform_title)
+        )
+        self.wait()
 
-        self.anim(nb_relevant_coins, idle_part, left_idle_part, last_coin_seq)
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        square = Square()
+        square.flip(RIGHT)
+        square.rotate(-3 * TAU / 8)
+        circle.set_fill(PINK, opacity=0.5)
+
+        self.play(ShowCreation(square))
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))
 
 
+class WarpSquare(Scene):
+    def construct(self):
+        square = Square()
+        self.play(ApplyPointwiseFunction(
+            lambda point: complex_to_R3(np.exp(R3_to_complex(point))),
+            square
+        ))
+        self.wait()
 
+
+class WriteStuff(Scene):
+    def construct(self):
+        example_text = TextMobject(
+            "This is a some text",
+            tex_to_color_map={"text": YELLOW}
+        )
+        example_tex = TexMobject(
+            "\\sum_{k=1}^\\infty {1 \\over k^2} = {\\pi^2 \\over 6}",
+        )
+        group = VGroup(example_text, example_tex)
+        group.arrange(DOWN)
+        group.set_width(FRAME_WIDTH - 2 * LARGE_BUFF)
+
+        self.play(Write(example_text))
+        self.play(Write(example_tex))
+        self.wait()
+
+
+class UpdatersExample(Scene):
+    def construct(self):
+        decimal = DecimalNumber(
+            0,
+            show_ellipsis=True,
+            num_decimal_places=3,
+            include_sign=True,
+        )
+        square = Square().to_edge(UP)
+
+        decimal.add_updater(lambda d: d.next_to(square, RIGHT))
+        decimal.add_updater(lambda d: d.set_value(square.get_center()[1]))
+        self.add(square, decimal)
+        self.play(
+            square.to_edge, DOWN,
+            rate_func=there_and_back,
+            run_time=5,
+        )
+        self.wait()
+
+# See old_projects folder for many, many more
